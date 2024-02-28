6bcfcaf7d76ccbbd968f85ac30119eb8c049ac04
Andres Fernandez <50086557+Andres-Fernandez-Caballero@users.noreply.github.com>
2024-02-28 20:58:23 +0000

    Add payment resourse

---
+++ app/Models/Payment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'payment_date_open',
+        'payment_date_paid',
+        'total_amount',
+        'student_id',
+        ];
+
+    public function isPaid():Attribute
+    {
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => $attributes['payment_date_paid'] != null
+        );
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
---
+++ database/migrations/2024_02_26_202456_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->date('payment_date_open');
+            $table->date('payment_date_paid')->nullable();
+
+            $table->decimal('total_amount', 10,2);
+            
+            $table->foreignId('student_id')
+                ->constrained('students')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
