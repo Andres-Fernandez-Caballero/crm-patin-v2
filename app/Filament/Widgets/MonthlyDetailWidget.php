74685fedbd7794d3407acf49432eb59dc73db3eb
Andres Fernandez <50086557+Andres-Fernandez-Caballero@users.noreply.github.com>
2024-03-01 15:12:57 +0000

    FIX monthlmonthly

--- app/Filament/Widgets/MonthlyDetailWidget.php
+++ app/Filament/Widgets/MonthlyDetailWidget.php
@@ -6,27 +6,21 @@ use App\Models\Payment;
 use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Log;
 
 class MonthlyDetailWidget extends BaseWidget
 {
     protected function getStats(): array
     {
+        Log::alert(Carbon::now()->month()->format('Y-m'));
         return [
             Stat::make(
                 
                 label: 'Cuotas abonadas ',
-                value: Payment::whereMonth('payment_date_open', Carbon::parse(Carbon::now()->month()->format('Y-m')))
-                ->where('payment_date_paid', '!=', null)
+                value: Payment::whereMonth('payment_date_open', Carbon::now())
+
                 ->sum('total_amount')
                 )
-                ->color('warning')
-
-                ->description(
-                    'Cuotas totales $' . Payment::whereMonth('payment_date_open', Carbon::parse(Carbon::now()->month()->format('Y-m')))
-                    // ->where('payment_date_paid', null)
-                ->sum('total_amount')
-                
-            )
         ];
     }
 }
